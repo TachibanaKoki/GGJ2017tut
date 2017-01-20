759642a383e76f46e30abf5b91fe34177c49e95f
massie0414 <massie0414@gmail.com>
2017-01-21 05:12:06 +0900

    一定時間経過でゲーム終了（まだ仮実装）

---
+++ Assets/Kato/Script/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    private float endTime = 3.0f;
+    private float nowTime = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        nowTime += Time.deltaTime;
+        Debug.Log( "nowTime="+nowTime );
+        if ( endTime < nowTime )
+        {
+            Debug.Log( "終わり" );
+        }
+    }
+}
