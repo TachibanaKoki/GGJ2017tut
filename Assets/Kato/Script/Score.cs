e296c90f9140f1fd7ed4847c3c7bc04444f7e0ed
massie0414 <massie0414@gmail.com>
2017-01-21 15:54:57 +0900

    スコアと残りタイムのUI（テキスト）のUICanvas.prefabを追加

---
+++ Assets/Kato/Script/Score.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+    GameManager gameManager;
+
+    // Use this for initialization
+    void Start () {
+        gameManager = GameManager.Instance;
+        gameManager.setScore(0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        this.GetComponent<Text>().text = "スコア:" + gameManager.getScore();
+    }
+}
