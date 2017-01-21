080f0bf572767ecf3d8a8786b449234bd74709b5
ralsoon <smallkirby423@yahoo.co.jp>
2017-01-21 18:14:56 +0900

    ゲームオーバーシーンからそれぞれのシーンに移動するように

---
+++ Assets/Abe/GameOver/TapGotoScene.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class TapGotoScene : MonoBehaviour, IPointerDownHandler
+{
+    [SerializeField]
+    float delayTime;
+
+    [SerializeField]
+    string sceneName;
+
+    [SerializeField]
+    LoadSceneMode mode;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        StartCoroutine(GotoScene());
+    }
+
+    IEnumerator GotoScene()
+    {
+        yield return new WaitForSeconds(delayTime);
+        SceneManager.LoadScene(sceneName, mode);
+    }
+}
