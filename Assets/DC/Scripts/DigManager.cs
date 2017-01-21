efd47a4254b125cc461f6d8d7248e9a8c42b33b1
DC <m01143022f@edu.teu.ac.jp>
2017-01-21 12:11:08 +0900

    [追加]岩の挙動を追加したプレハブの追加

---
+++ Assets/DC/Scripts/DigManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DigManager : MonoBehaviour {
+
+    #region 変数格納
+
+    public MapManager man;
+    public GameObject ground;
+    public GameObject fossil;
+
+    public float tension = 1;
+
+    public float score = 100;
+    public float damagerate = 20;
+    public float minus = 50;
+
+    public float dig = 0;
+    public float digmax = 100;
+
+    public int x, y;
+
+    private float alpha = 0;
+
+    #endregion
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Dig(dig);
+        man.score[x, y] = ScoreMus();
+	}
+
+    public void Dig(float a) {
+        alpha = a * tension / 100;
+        fossil.GetComponent<SpriteRenderer>().color = new Color(1.0f, 1.0f, 1.0f, alpha);
+    }
+
+    public float ScoreMus() {
+        float res = 0;
+
+        if (dig > digmax) {
+            res = score;
+            if (dig > digmax + damagerate) {
+                res -= Mathf.Floor((dig - digmax) / damagerate) * minus;
+                man.map[x, y] = true;
+            }
+        }
+
+        if (res < 0) {
+            res = 0;
+            man.map[x, y] = false;
+        }
+
+        return res;
+    }
+}
---
+++ Assets/DC/Scripts/MapManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Set {
+    public int x, y;
+};
+
+[System.Serializable]
+public class Pare {
+    public Set[] set;
+};
+
+public class MapManager : MonoBehaviour {
+
+    public int mapx = 3;
+    public int mapy = 3;
+    public bool[,] map;
+    public float[,] score;
+
+    public Pare[] pare;
+
+    private int num;
+
+	// Use this for initialization
+	void Start () {
+        map = new bool[mapx, mapy];
+        score = new float[mapx, mapy];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void CompareSet() {
+
+    }
+}
