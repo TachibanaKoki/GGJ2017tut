c28f20eb6e69462c278e10710aa7222f9c298042
DC <m01143022f@edu.teu.ac.jp>
2017-01-21 18:06:53 +0900

    [変更]岩プレハブへのイメージの付与

--- Assets/DC/Scripts/RockControle.cs
+++ Assets/DC/Scripts/RockControle.cs
@@ -7,13 +7,20 @@ public class RockControle : MonoBehaviour {
     public float hp = 100;
     public bool flag = false;
 
+    public Sprite first, second, third;
+
+    public float maxhp;
+
 	// Use this for initialization
 	void Start () {
         flag = false;
+        maxhp = hp;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        ChangeSprite();
+
         if (hp < 0) {
             Destroy(this.gameObject);
         }
@@ -33,6 +40,24 @@ public class RockControle : MonoBehaviour {
     public void OnTriggerExit2D(Collider2D col) {
         if (col.gameObject.tag == "Player") {
             flag = false;
+        }
+    }
+
+    public void ChangeSprite() {
+        if (hp < maxhp / 3)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = third;
+            return;
+        }
+        else if (hp > maxhp / 3 * 2)
+        {
+            this.GetComponent<SpriteRenderer>().sprite = first;
+            return;
+        }
+        else
+        {
+            this.GetComponent<SpriteRenderer>().sprite = second;
+            return;
         }
     }
 }
---
+++ Assets/DC/Scripts/Temp.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Temp : MonoBehaviour {
+
+    public float delay;
+    public float tempo;
+    public float min;
+    public float max;
+
+    private float start;
+    private float end;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
