6d5112901de7966df6b874263a1e965a0d862aa1
DC <m01143022f@edu.teu.ac.jp>
2017-01-21 18:49:10 +0900

    [追加]リズム機能の追加

--- Assets/DC/Scripts/Temp.cs
+++ Assets/DC/Scripts/Temp.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Temp : MonoBehaviour {
 
@@ -10,15 +11,51 @@ public class Temp : MonoBehaviour {
     public float max;
 
     private float start;
-    private float end;
+    private float now;
 
-	// Use this for initialization
-	void Start () {
-		
+    public bool flag;
+
+    public int combo;
+
+    #region フレーム処理
+
+    // Use this for initialization
+    void Start () {
+        now -= delay;
+        TapUtils.I.OnTapDown += TempoTest;
+        TapUtils.I.OnTapDown += Combo;
+        flag = false;
+        combo = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        now++;
 	}
+
+    public void Combo(Vector3 vec) {
+        if (flag == true)
+        {
+            combo++;
+        }
+        else {
+            combo = 0;
+        }
+    }
+
+    public void TempoTest(Vector3 vec) {
+        Debug.Log(now % tempo + max);
+
+
+        if (now % tempo < Math.Abs(min) || tempo < now % tempo + max)
+        {
+            flag = true;
+        }
+        else
+        {
+            flag = false;
+        }
+    }
+
+    #endregion
 }
