ed0f7993994ed5c823ddfec10bd42c3edc5e0d99
ralsoon <smallkirby423@yahoo.co.jp>
2017-01-21 13:11:03 +0900

    タイトル画面でタップしてシーン移動まで

---
+++ Assets/Abe/GameOver/RandomSpawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RandomSpawner : MonoBehaviour
+{
+    [SerializeField, Tooltip("岩のプレハブ")]
+    List<GameObject> stones;
+
+    [SerializeField, Tooltip("生成する数"), Range(1, 50)]
+    int   instancenumbers;
+
+    [SerializeField, Tooltip("生成の間隔"), Range(0, 5)]
+    float intervalTime;
+    
+    void Awake()
+    {
+        
+    }
+    
+    void Start()
+    {
+        
+    }
+}
---
+++ Assets/Abe/Other/Script/TapPlayAnimation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TapPlayAnimation : MonoBehaviour, IPointerDownHandler
+{
+    [SerializeField]
+    GameObject obj;
+
+    //タップ
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Animator anim = obj.GetComponent<Animator>();
+        anim.Play("TapAnimation");
+
+        
+    }
+}
