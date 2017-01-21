cc7aa4f6775f7122b8234610e71914e6099cb922
massie0414 <massie0414@gmail.com>
2017-01-22 07:57:14 +0900

    ゲームオーバー画面のボタンをボタンらしくしました。

---
+++ Assets/Abe/Other/Script/TapGotoSceneButton.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class TapGotoSceneButton : MonoBehaviour
+{
+    [SerializeField]
+    float delayTime;
+
+    [SerializeField]
+    string sceneName;
+
+    [SerializeField]
+    LoadSceneMode mode;
+
+    public void OnClick()
+    {
+        switch (PlayerPrefs.GetInt("stage"))
+        {
+            case 1:
+                SceneManager.LoadScene("Master");
+                break;
+            case 2:
+                SceneManager.LoadScene("Master 1");
+                break;
+            case 3:
+                SceneManager.LoadScene("Master 2");
+                break;
+            default:
+                SceneManager.LoadScene("TitleScene");
+                break;
+        }
+        if (sceneName == "TitleScene")
+        {
+            SceneManager.LoadScene(sceneName, mode);
+        }
+    }
+}
