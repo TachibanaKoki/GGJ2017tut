212373e83410de98449226bc9e999ac9cf4e572b
ralsoon <smallkirby423@yahoo.co.jp>
2017-01-21 15:33:31 +0900

    ゲームオーバーシーンを編集

--- Assets/Abe/GameOver/RandomSpawner.cs
+++ Assets/Abe/GameOver/RandomSpawner.cs
@@ -8,7 +8,7 @@ public class RandomSpawner : MonoBehaviour
     List<GameObject> stones;
 
     [SerializeField, Tooltip("生成する数"), Range(1, 50)]
-    int   instancenumbers;
+    int   instanceNumbers;
 
     [SerializeField, Tooltip("生成の間隔"), Range(0, 5)]
     float intervalTime;
@@ -18,8 +18,23 @@ public class RandomSpawner : MonoBehaviour
         
     }
     
-    void Start()
+    IEnumerator Start()
     {
-        
+        for(int i = 0; i < instanceNumbers; i++)
+        {
+            int rand = Random.Range(0, stones.Count);
+            GameObject randomStone = stones[rand];
+
+            GameObject instance = Instantiate(randomStone, transform.position, Quaternion.identity);
+            Rigidbody2D rig = instance.GetComponent<Rigidbody2D>();
+            Vector2 force = new Vector2()
+            {
+                x = Random.Range(-1.0f, 1.0f),
+                y = Random.Range( 0.0f, 1.0f)
+            };
+            rig.AddForce(force * 50, ForceMode2D.Impulse);
+
+            yield return new WaitForSeconds(intervalTime);
+        }
     }
 }
