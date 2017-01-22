90779da3e2181e670226865bd906f06a1d6e4b25
ralsoon <smallkirby423@yahoo.co.jp>
2017-01-22 11:10:44 +0900

    チュートリアルを追加　仮実装

---
+++ Assets/Abe/Tutorial/ShowTutorial.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ShowTutorial : MonoBehaviour
+{
+    [System.Serializable]
+    struct Tutorial
+    {
+        string playAnimation;
+    }
+
+    void Awake()
+    {
+        
+    }
+    
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
